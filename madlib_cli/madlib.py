4fff7f71e073a0c5762a2c9dde31c5392dd06e63
lana-z <lana@levelupeconomy.com>
2024-01-11 14:25:15 -0800

    tests passing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -15,33 +15,31 @@ def read_template (path):
         return contents.strip()
 
 
-def parse_template():
-
-    """
-    Given "It was a {Adjective} and {Adjective} {Noun}." Return "It was a {} and {} {}." i.e. takes in a stripped template string and returns a string with language parts removed and a separate list of those language parts.
-    """
+def parse_template(template):
     # return "It was a {} and {} {}.", ("Adjective", "Adjective", "Noun")
-
-    """
-     It was a {Adjective} and {Adjective} {Noun}.
-              ^capture mode
-     It was a { 
-     
-     capture Adjective and keep going along string
-     ["Adjective", "Adjective", "Noun"] - list
-     ("Adjective," "Adjective," "Noun") - tuple
-    """
-
-
     parts = []
+    stripped = ""
+
+    capturing = False
     capture = ""
-    for word in .split():
-        if word.startswith("{"):
-            parts.append(word.strip(".,?!"))
-            capture += "{} "
+
+    for char in template:
+        if capturing:
+            if char == "}":
+                parts.append(capture)
+                capture = ""
+                capturing = False
+                stripped += "{}"
+            else:
+                capture += char
         else:
-            capture += word + " "
-    return capture.strip(), tuple(parts) 
+            if char != "{":
+                stripped += char
+
+            if char == "{":
+                capturing = True
+
+    return stripped, tuple(parts) 
 
 
 def merge(stripped_template, parts):
@@ -52,18 +50,3 @@ def merge(stripped_template, parts):
     # * unpacks the tuple ie. turns into separate arguments - called star args or splat args
     return stripped_template.format(*parts)
 
-
-# def main():
-#     welcome()
-#     template, placeholders = parse_template('assets/dark_and_stormy_night_template.txt')
-#     user_inputs = [input(f"Enter a {placeholder}: ") for placeholder in placeholders]
-#     madlib = template.format(*user_inputs)
-#     print(madlib)
-#     write_to_file(madlib)
-
-# def write_to_file(madlib):
-#     with open('completed_madlib.txt', 'w') as file:
-#         file.write(madlib)
-
-# if __name__ == '__main__':
-#     main()
